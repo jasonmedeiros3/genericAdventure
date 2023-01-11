7932e1030e8971a767bf4c8aca2abc7f1b1d8966
charlesqiu137 <122378037+charlesqiu137@users.noreply.github.com>
2023-01-11 14:37:45 -0500

    Finished implementing all Floor 3 bosses

--- Boss.java
+++ Boss.java
@@ -12,9 +12,11 @@ public class Boss {
 	private int poison=0;
 	private int markedForDeath=0;
 	private int unaware=0;
-	private int weight;
-	private int freeze;
-	private int invuln;
+	private int freeze=0;
+	private int invuln=0;
+	private int sniperCharge=0;
+	private int compactorCounter=9;
+	private boolean huntingShotgun=false;
 	public Boss(String biome) {
 		Random rand=new Random();
 		int seed=rand.nextInt(101);
@@ -52,121 +54,351 @@ public class Boss {
 				else {
 					nameSelector("The Abstract Concept");
 				}
+				break;
+			case 10:
+				nameSelector("The Devil");
+				break;
+			case 0:
+				nameSelector("The Truth");
 		}
 	}
 	public void nameSelector(String n) {
 		name=n;
 		switch(name) {
-			case "Squirrel":
-				maxhp=(int)(30+1.9*(Math.pow(Floor.level-1,1.3)));
-				atk=(int)(90+7.2*(Floor.level-1));
-				def=(int)(100+1.6*Math.pow(Floor.level-1,1.4));
-				setWeight(1);
-				break;
-			case "Branch Manager":
-				maxhp=(int)(65+5*Math.pow(Floor.level,1.09));
-				atk=(int)(107+1.1*Floor.level);
-				def=(int)(90+3*Floor.level);
-				setWeight(2);
-				break;
-			case "Cardboard Deer":
-				maxhp=(int)(5+3*Math.pow(Floor.level,1.08));
-				atk=(int)(87+9.1*Math.pow(Floor.level,0.9));
-				def=(int)(93+0.5*Floor.level);
-				setWeight(1);
-				break;
-			case "Semi-Sentient Tree":
-				maxhp=(int)(220+15.5*Math.pow(Floor.level,1.2));
-				atk=(int)(60+3.2*Floor.level);
-				def=(int)(100+0.35*Floor.level);
-				setWeight(4);
-				break;
-			case "Poacher":
-				maxhp=(int)(109+0.6*Floor.level);
-				atk=(int)(100+1.5*Math.pow(Floor.level,1.44));
-				def=(int)(103-0.7*Math.pow(Floor.level,1.21));
-				setWeight(4);
-				break;
-			case "Clown":
-				maxhp=(int)(133+3.25*Math.pow(Floor.level,1.34));
-				atk=(int)(107+0.9*Floor.level);
-				def=(int)(77+15*Math.sqrt(Floor.level));
-				setWeight(3);
-			case "Defective Clown":
-				maxhp=1;
-				atk=(int)(112+3.6*Math.pow(Floor.level, 1.15));
+			case "The Sniper":
+				maxhp=375;
+				atk=107;
 				def=100;
-				setWeight(2);
-			case "Assembly Line Machine":
-				maxhp=(int)(300+10*Math.pow(Floor.level,1.45));
-				atk=(int)(90+1.7*Math.pow(Floor.level-1,1.03));
-				def=(int)(107+Floor.level);
-				setWeight(5);
-			case "Weaponized Circus Drone":
-				maxhp=(int)(40+2.7*Math.pow(Floor.level-1,1.28));
-				atk=(int)(100+2.5*Floor.level);
-				def=(int)(85+4*Floor.level);
-				setWeight(2);
-			case "Polar Camel":
-				maxhp=(int)(133+3.3*Math.pow(Floor.level,1.2));
-				atk=(int)(80+2.9*Math.pow(Floor.level,1.05));
-				def=(int)(93+1.1*Floor.level);
-				setWeight(3);
-			case "Psychrophilic Amphibious Jaguar":
-				maxhp=(int)(95+5*Math.pow(1.3*Floor.level, 1.05));
-				atk=(int)(100+0.5*Floor.level);
-				def=(int)(97+0.4*Floor.level);
-				setWeight(3);
-			case "Cheese-Coated Tourist":
-				maxhp=(int)(38+Math.pow(1.2*Floor.level, 1.09));
-				atk=(int)(95+2.4*(Floor.level-1));
-				def=(int)(100+Floor.level);
-				setWeight(1);
-			case "Completely Normal Shark":
-				maxhp=(int)(96+Floor.level);
-				atk=(int)(66+17*Math.sqrt(Floor.level));
-				def=(int)(98+1.2*Floor.level);
-				setWeight(2);
-			case "Lawyer":
-				maxhp=(int)(65+1.23*Math.pow(1.23*Floor.level,1.23));
-				atk=(int)(100+Math.sqrt(3*Floor.level));
-				def=(int)(93);
-				setWeight(2);
-			case "Consul":
-				maxhp=(int)(80+3*Floor.level);
-				atk=(int)(91+1.1*Floor.level);
-				def=(int)(93+Floor.level);
-				setWeight(2);
-			case "Consulate Janitor":
-				maxhp=(int)(107+1.8*Floor.level);
-				atk=(int)(80+1.25*Math.pow(Floor.level,1.25));
-				def=(int)(97+0.9*Floor.level);
-				setWeight(3);
-			case "Sapient Grapevine":
-				maxhp=(int)(90+1.05*Math.pow(Floor.level,1.4));
-				atk=(int)(77+1.13*Math.pow(Floor.level,1.23));
-				def=(int)(107+1.1*Floor.level);
-				setWeight(3);
-			case "":
+				break;
+			case "The Druid":
+				maxhp=500;
+				atk=93;
+				def=110;
+				break;
+			case "The Trash Compactor":
+				maxhp=575;
+				atk=80;
+				def=117;
+				break;
+			case "The Cube of Gelatin":
+				maxhp=400;
+				atk=93;
+				def=107;
+				break;
+			case "The British Guy":
+				maxhp=480;
+				atk=100;
+				def=95;
+				break;
+			case "The Engineer":
+				maxhp=500;
+				atk=100;
+				def=109;
+				break;
+			case "The Fighter":
+				maxhp=780;
+				atk=112;
+				def=101;
+				break;
+			case "The Supercomputer":
+				maxhp=675;
+				atk=93;
+				def=108;
+				break;
+			case "The Ophan":
+				maxhp=777;
+				atk=107;
+				def=97;
+				break;
+			case "The Abstract Concept":
+				maxhp=10;
+				atk=100;
+				def=100;
+				break;
+			case "The Devil":
+				maxhp=1998;
+				atk=66;
+				def=66;
+				break;
+			case "The Truth":
+				maxhp=1;
+				atk=1;
+				def=1;
+				break;
+			default:
+				n="File A Bug Report Please";
+				maxhp=100;
+				atk=100;
+				def=100;
 		}
 		hp=maxhp;
 	}
 	public void damage(int i) {
 		int damage;
-		damage=i*(100/def);
+		damage=(int)(i*(100.0/def));
 		if(getMarkForDeath()>0&&damage>0) {
 			damage*=1.35;
 		}
 		if(damage>0) {
-			System.out.println("Did "+damage+" damage.");
+			System.out.println("Did "+damage+" damage to "+name+".");
 		}
 		else if(damage<0) {
-			System.out.println("Enemy "+name+" healed "+(-damage)+" health.");
+			System.out.println(name+" healed "+(-damage)+" health.");
 		}
 		else {
 			System.out.println("Didn't do any damage.");
 		}
 		hp-=damage;
+	}
+	public void damage(double i) {
+		int damage;
+		damage=(int)(i*(100.0/def));
+		if(getMarkForDeath()>0&&damage>0) {
+			damage*=1.35;
+		}
+		if(damage>0) {
+			System.out.println("Did "+damage+" damage to "+name+".");
+		}
+		else if(damage<0) {
+			System.out.println(name+" healed "+(-damage)+" health.");
+		}
+		else {
+			System.out.println("Didn't do any damage.");
+		}
+		hp-=damage;
+	}
+	public void doMove(Player player) {
+		Random rand=new Random();
+		int seed1=rand.nextInt(100);
+		int seed2=rand.nextInt(100);
+		if(freeze>0) {
+			System.out.println(name+" is frozen.");
+		}
+		else {
+			switch(name) {
+				case "The Sniper":
+					if(seed1<=20||player.getHp()<=(40*atk/103)||(seed1<=90&&sniperCharge>=3)) {
+						noScope(player,seed2);
+					}
+					else if((seed1<=49||player.getMarkForDeath()>0&&sniperCharge<=0)||seed1<=22) {
+						bushwacka(player);
+					}
+					else if(seed2<=2||(seed1<=60&&player.getMarkForDeath()<=0&&sniperCharge<=0)||(seed1<=30&&sniperCharge==1)) {
+						jarate(player);
+					}
+					else if((seed1<=87&&unaware<=0&&sniperCharge<=0)||seed2==69) {
+						razorback();
+					}
+					else {
+						charge();
+					}
+				case "The Druid":
+					if(seed1<=90&&(poison>0||markedForDeath>0)) {
+						aromatherapy(seed2);
+					}
+					else if(seed1<=15||(seed2<=80&&hp<=maxhp/8.5&&player.getHp()>=30)) {
+						smokeWeed(seed2);
+					}
+					else if(seed1<=40||(seed1<=75&&!huntingShotgun)) {
+						huntingShotgun(player,seed2);
+					}
+					else if(seed1<=80||(seed1<=99&&player.getHp()<35)) {
+						beehive(player);
+					}
+					else {
+						poisonOak(player);
+					}
+				case "The Trash Compactor":
+					if((seed1<=20&&hp<=300)||afterburn>0||poison>0||markedForDeath>0||(seed1<=50&&hp<=125)) {
+						selfRepair();
+					}
+					else {
+						compact(player);
+					}
+				case "The Cube of Gelatin":
+					if(seed1<=45) {
+						gelatinPunch(player);
+					}
+					else if(seed1<=70||(seed1+(25-Room.turn*Room.turn)>=99)) {
+						gelatinHarden();
+					}
+					else {
+						gelatinTherapy();
+					}
+				case "The British Guy":
+					if(Room.turn%5==0) {
+						teaBreak(player);
+					}
+					else if(seed1<=40) {
+						shivUp(player);
+					}
+					else if(seed1<=65) {
+						godSaveTheKing(player);
+					}
+					else if(seed1<=80&&player.getFreeze()<=0) {
+						ateTheWeatherSimpleAs(player,seed2);
+					}
+					else {
+						lunch(player);
+					}
+			}
+		}
+	}
+	private void teaBreak(Player player) {
+		System.out.println(name+" calls a tea break.");
+		System.out.println("Tea. Earl Grey. Hot.");
+		player.damage(-10);
+		damage(-10);
+	}
+	private void shivUp(Player player) {
+		System.out.println(name+" shivs you up.");
+		System.out.println("The knife is so rusty that you get poisoned.");
+		player.damage(40*atk/100);
+		player.setPoison(1);
+	}
+	private void godSaveTheKing(Player player) {
+		System.out.println(name+" plays the British national anthem.");
+		player.damage(50*atk/100);
+	}
+	private void ateTheWeatherSimpleAs(Player player,int seed) {
+		System.out.println(name+" says \"'ate the weather, simple as.\"");
+		System.out.println("On cue, a frigid rainstorm rolls in.");
+		if(seed>=70) {
+			System.out.println("This is what London is like all the time.");
+		}
+		player.setFreeze(2);
+	}
+	private void lunch(Player player) {
+		System.out.println(name+" shares a traditional British delicacy with you.");
+		System.out.println("It's stale toast with canned beans.");
+		player.damage(25);
+		damage(-25);
+	}
+	private void gelatinPunch(Player player) {
+		System.out.println("Somehow, "+name+" punches you.");
+		player.damage(40*atk/100);
+	}
+	private void gelatinHarden() {
+		System.out.println(name+" hardened.");
+		System.out.println("Its defense went up.");
+		def+=6;
+	}
+	private void gelatinTherapy() {
+		System.out.println(name+" attended cognitive behavioural therapy.");
+		System.out.println(name+" healed on the inside.");
+		damage(-37);
+	}
+	private void selfRepair() {
+		damage(-25);
+		if(afterburn>0) {
+			afterburn--;
+		}
+		if(poison>0) {
+			poison--;
+		}
+		if(markedForDeath>0) {
+			markedForDeath--;
+		}
+	}
+	private void compact(Player player) {
+		compactorCounter--;
+		System.out.println("COMPACTION COUNTER: "+compactorCounter);
+		if(compactorCounter<=0) {
+			System.out.println("COMPACTING- VACATE COMPACTION AREA IMMEDIATELY");
+			player.damage(10000);
+			compactorCounter=6;
+		}
+	}
+	private void aromatherapy(int seed) {
+		if(seed<=42) {
+			System.out.println(name+" used aromatherapy despite not being a licensed practitioner.");
+		}
+		else {
+			System.out.println(name+" used aromatherapy.");
+		}
+		poison=0;
+		markedForDeath=0;
+	}
+	private void smokeWeed(int seed) {
+		if(seed<=84) {
+			System.out.println(name+" smokes some weed.");
+		}
+		else {
+			System.out.println(name+" imbibes in the worldly pleasures of marijuana usage.");
+		}
+		if(seed>=77) {
+			System.out.println("This is probably illegal.");
+		}
+		damage(-20);
+		unaware+=2;
+	}
+	private void huntingShotgun(Player player,int seed) {
+		if(!huntingShotgun) {
+			System.out.println(name+" picks up a double barrel hunting shotgun.");
+			System.out.println(name+" has poor trigger discipline.");
+			huntingShotgun=true;
+		}
+		else {
+			System.out.println(name+" fires the hunting shotgun. The recoil is significant.");
+			player.damage(90*atk/100.0);
+			damage(30);
+		}
+	}
+	private void beehive(Player player) {
+		System.out.println(name+" throws a beehive at you. No bees, but it still does damage.");
+		player.damage(39*atk/97.0);
+	}
+	private void poisonOak(Player player) {
+		System.out.println(name+" swings a piece of poison oak.");
+		System.out.println("You become poisoned by unrelated circumstances.");
+		player.damage(44*atk/100);
+		player.setPoison(3);
+	}
+	private void noScope(Player player,int seed) {
+		System.out.println(name+" used the 180 No-Scope.");
+		if(seed<=31) {
+			System.out.println("Despite the gun facing away from you, you get hit anyway.");
+		}
+		else if(seed<=35) {
+			System.out.println("Despite the gun facing away from you, you get hit anyway...?");
+		}
+		player.damage((45+sniperCharge*15)*atk/102.0);
+		sniperCharge=0;
+	}
+	private void bushwacka(Player player) {
+		sniperCharge=0;
+		System.out.println(name+" used the Bushwacka.");
+		if(player.getMarkForDeath()>0) {
+			System.out.println("A critical hit.");
+			player.damage(93*atk/100.0);
+		}
+		else {
+			player.damage(31*atk/100.0);
+		}
+	}
+	private void jarate(Player player) {
+		sniperCharge=0;
+		System.out.println(name+" used the Jarate.");
+		if(player.getMarkForDeath()<=0) {
+			System.out.println("You are now covered in piss.");
+			System.out.println("Will to live went down.");
+			player.setMarkForDeath(3);
+		}
+		else {
+			System.out.println("The number of piss on you increased by one.");
+			player.setMarkForDeath(1);
+		}
+	}
+	private void razorback() {
+		sniperCharge=0;
+		System.out.println(name+" used the Razorback.");
+		System.out.println(name+" became invulnerable for 1 turn and unaware for 2 turns.");
+		invuln++;
+		unaware+=2;
+	}
+	private void charge() {
+		System.out.println(name+" is charging up a shot.");
+		sniperCharge++;
 	}
 	public void setAtk(int increment) {
 		atk+=increment;
@@ -224,12 +456,6 @@ public class Boss {
 	}
 	public int getInvuln() {
 		return invuln;
-	}
-	public int getWeight() {
-		return weight;
-	}
-	public void setWeight(int weight) {
-		this.weight = weight;
 	}
 	public void statusTick() {
 		if(afterburn>0) {
